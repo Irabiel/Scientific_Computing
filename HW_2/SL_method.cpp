900f3ca94d4da6aef225d8f35e9adcfc3180d062
Irabiel <irabiel@yahoo.com>
2022-10-11 01:19:40 -0700

    working on HW

--- HW_2/SL_method.cpp
+++ HW_2/SL_method.cpp
@@ -3,8 +3,32 @@
 //
 
 #include "SL_method.h"
+#include "cmath"
 
 
+std::vector<double> compute_vel(double x, double y){
+    std::vector<double> vel_temp ;
+    vel_temp.assign(2, 0.);
+    vel_temp[0] = -y;
+    vel_temp[1] = x;
+}
+
+void SL_method::sol_IC(){
+    int nx = sl_grid.get_numb_x();
+    int ny = sl_grid.get_numb_y();
+    sol_old.assign(nx*ny, 0.);
+
+    for (int i = 0; i < (nx-1)*(ny-1); i++){
+        sol_old[i] = sqrt((sl_grid.x_from_n(i) - 0.25)*(sl_grid.x_from_n(i) - 0.25) + sl_grid.x_from_n(i)*sl_grid.x_from_n(i)) - 0.2;
+    }
+
+}
+
+void SL_method::update_sol_old(){
+    for (int i = 0; i < sol_old.size() - 1; i++){
+        sol_old[i] = sol[i];
+    }
+}
 
 void SL_method::find_trajectory(int n, double &x_d, double &y_d, double dt) {
     // RK1 Euler Method
@@ -15,11 +39,42 @@ void SL_method::find_trajectory(int n, double &x_d, double &y_d, double dt) {
 }
 
 void SL_method::find_trajectoryRK2(int n, double &x_d, double &y_d, double dt) {
-    // RK1 Euler Method
+    // RK2 Euler Method
     double x_0 = sl_grid.x_from_n(n);
     double y_0 = sl_grid.y_from_n(n);
-    double x_temp = 0.;
-    double y_temp = 0.;
-    x_d = x_0 - dt * vel_u[n];
-    y_d = y_0 - dt * vel_v[n];
+    x_temp = compute_vel(x_0, y_0);
+    x_star = x_0 - .5*dt * x_temp[0];
+    y_star = y_0 - .5*dt * x_temp[1];
+    x_temp = compute_vel(x_star, y_star);
+    x_d = x_0 - dt * x_temp[0];
+    y_d = y_0 - dt * x_temp[1];
+}
+
+void SL_method::Solver(double dt, double Tf) {
+
+int nx = sl_grid.get_numb_x();
+int ny = sl_grid.get_numb_y();
+sol.assign(nx * ny, 0.);
+
+std::vector<double> xd;
+std::vector<double> yd;
+xd.assign(nx, 0.);
+yd.assign(ny, 0.);
+
+// need to work on this more. Might be wrong
+for (int i = 0; i < (nx)*(ny); i++){
+    find_trajectoryRK2( i, xd[i], yd[i],  dt);
+}
+
+int nt = ceil(Tf/dt);
+sol_IC();
+
+for (int n = 1; n < nt; n++){
+    for (int i = 0; i < nx; i++)
+        for (int j = 0; j < ny; j++)
+            sol[i+j] = quadratic_interpolation(sl_grid, sol_old, xd[i], yd[j]);
+    update_sol_old();
+}
+
+
 }
--- HW_2/SL_method.h
+++ HW_2/SL_method.h
@@ -6,6 +6,7 @@
 #define F22_MATH_233_LABS_SL_METHOD_H
 
 #include "Grid2d.h"
+#include "math_tools.h"
 #include <vector>
 
 // Semi-Langrangian Method
@@ -13,17 +14,23 @@ class SL_method {
 private:
     Grid2d sl_grid;
     std::vector<double> sol;
+    std::vector<double> sol_old;
     std::vector<double> vel_u;
     std::vector<double> vel_v;
+    std::vector<double> x_temp;
+    double x_star;
+    double y_star;
     void find_trajectory(int n, double & x_d, double & y_d, double dt);
     void find_trajectoryRK2(int n, double & x_d, double & y_d, double dt);
-    void reset_temp();
+    void sol_IC();
+    void update_sol_old();
+    std::vector<double> compute_vel(double x, double y);
 
 public:
     void set_grid(Grid2d & new_grid){sl_grid = new_grid;} // set grid
     std::vector<double> get_sol(){ return sol; }        // access solution
     void set_velocity(std::vector<double> & vel_u0, std::vector<double> & vel_v0); // for constant velocity
-
+    void Solver(double dt, double Tf);
 };
 
 
